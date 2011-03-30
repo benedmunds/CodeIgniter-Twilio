d578e095aa476c288d588c055e78913e5c4526ad
Ben Edmunds <ben.edmunds@gmail.com>
2011-03-30 17:16:50 -0500

    Initial commit

---
+++ config/twilio.php
@@ -0,0 +1,44 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	/**
+	* Name:  Twilio
+	*
+	* Author: Ben Edmunds
+	*		  ben.edmunds@gmail.com
+	*         @benedmunds
+	*
+	* Location:
+	*
+	* Created:  03.29.2011
+	*
+	* Description:  Twilio configuration settings.
+	*
+	*
+	*/
+
+	/**
+	 * Mode ("sandbox" or "prod")
+	 **/
+	$config['mode']   = 'sandbox';
+
+	/**
+	 * Account SID
+	 **/
+	$config['account_sid']   = '';
+
+	/**
+	 * Auth Token
+	 **/
+	$config['auth_token']    = '';
+
+	/**
+	 * API Version
+	 **/
+	$config['api_version']   = '2010-04-01';
+
+	/**
+	 * Twilio Phone Number
+	 **/
+	$config['number']        = '';
+
+
+/* End of file twilio.php */
